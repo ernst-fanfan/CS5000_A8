18f85442fe60dc97f4eba1ca98e37e3822a583ba
Ernst Fanfan <11448222+ernst-fanfan@users.noreply.github.com>
2019-10-30 14:27:24 -0400

    Added toString to account standard interest rate as a decimal Added DecimalFormat for both percent and dollar

--- Account.java
+++ Account.java
@@ -5,51 +5,60 @@
 //Assignment: 8
 //IDE Name: IntelliJ
 
+import java.text.DecimalFormat;
 import java.util.Date;
+
 public class Account{
     private int id = 0;//Account number
     private double balance = 0.0;//Account current balance
-    private static double annualInterestRate = 0;//Interest Rate
+    private double annualInterestRate = 0;//Interest Rate
     private Date dateCreated;//Date created
 
     public Account() {//Non-Argument constructor
         Date date = new Date();
     }
     public Account(int id, double balance){//constructor
-        this.id = id;
-        this.balance = balance;
-        dateCreated = new Date();
+        this.id = id;//account ID
+        this.balance = balance;//account balance
+        dateCreated = new Date();//date created
     }
 
     //fetch methods
     public int getId(){return id;}//ID fetch
     public double getBalance(){return balance;}//current balance fetch
-    public double getAnnualInterestRate(){return annualInterestRate;}//Interest rate fetch
+    public double getAnnualInterestRate(){return annualInterestRate*100;}//Interest rate fetch
     public String getDateCreated(){
         return dateCreated.toString();}//date fetch
     public double getMonthlyInterestRate(){//monthly rates fetch
         return ((annualInterestRate/12)*100);
-    }
+    }//monthly interest rate
     public double getMonthlyInterest(){
         return (balance*annualInterestRate);
-    }
+    }//monthly calculate interest
 
     //set methods
     public void setId(int id){//set Id
         this.id = id;
-    }
+    }//set ID
     public void setBalance(double balance){//set balance
         this.balance = balance;
-    }
+    }//set starting balance
     public void setAnnualInterestRate(double annualInterestRate){//set rate
-        this.annualInterestRate = annualInterestRate;
-    }
+        this.annualInterestRate = annualInterestRate/100;
+    }//set annual interest rate
 
     //mutator methods
     public void withdraw(double amount){
         balance = balance-amount;
-    }
+    }//reduce balance
     public void deposit(double amount){
         balance =balance+amount;
+    }//increase balance
+
+    //to String method
+    public String toString (Account account){//relevant account info
+        DecimalFormat dollar = new DecimalFormat("$###,##0.00");
+        DecimalFormat percent = new DecimalFormat("#0.00%");
+        return ("Account ID:\t \t \t"+id+"\nAccount Balance:\t \t"+dollar.format(balance)+"\nInterest Rate:\t \t \t"+percent.format(getAnnualInterestRate())+"\nDate Opened:\t \t \t"+getDateCreated());
     }
 }
